19ade634bad41c6947940e8d382716eff3e28cfa
Cody Coons <cody@codycoons.com>
2014-03-12 11:44:01 -0400

    Updated go install to use godeb.

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Add go repository
-apt-get install -y python-software-properties
-add-apt-repository -y ppa:gophers/go
-apt-get update
-
-# Install go
-apt-get install -y golang-stable
-
 # Install source control
-apt-get install -y git-core subversion bzr
+apt-get install -y git-core subversion bzr mercurial
+
+# Download GODEB binar
+mkdir /tmp/godeb
+cd /tmp/godeb
+wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz
+tar xvzf godeb-amd64.tar.gz
+chmod +x godeb
+mv godeb /usr/local/bin/godeb
+
+# Install Go
+godeb install
 
 # Setup GOPATH
 mkdir /home/vagrant/gocode
 mkdir /home/vagrant/gocode/{pkg,bin,src}
 chown -R vagrant /home/vagrant/gocode
 echo 'export GOPATH=/home/vagrant/gocode' >> /home/vagrant/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin' >> /home/vagrant/.bashrc
